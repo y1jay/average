cf83ff8f15627e61e35bb8e4064112fe2bc74bda
Jun <67768207+y1jay@users.noreply.github.com>
2022-10-22 17:23:30 +0900

    초기설정 셋팅 2

--- App.js
+++ App.js
@@ -6,57 +6,60 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+	SafeAreaView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	View,
+} from "react-native";
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+	Colors,
+	DebugInstructions,
+	Header,
+	LearnMoreLinks,
+	ReloadInstructions,
+} from "react-native/Libraries/NewAppScreen";
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === "dark";
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+	const backgroundStyle = {
+		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+	};
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={{flex: 1, backgroundColor: 'white', color: 'white'}}></View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={backgroundStyle}>
+			<StatusBar
+				barStyle={isDarkMode ? "light-content" : "dark-content"}
+			/>
+			<View
+				style={{ flex: 1, backgroundColor: "white", color: "white" }}
+			></View>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+	sectionContainer: {
+		marginTop: 32,
+		paddingHorizontal: 24,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: "600",
+	},
+	sectionDescription: {
+		marginTop: 8,
+		fontSize: 18,
+		fontWeight: "400",
+	},
+	highlight: {
+		fontWeight: "700",
+	},
 });
 
 export default App;
