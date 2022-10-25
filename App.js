bc3358ff7917d7bb1dd183fe3d4997ab55eacab0
Jun <67768207+y1jay@users.noreply.github.com>
2022-10-25 21:57:14 +0900

    라이브러리 설치 및 초기셋팅

--- App.js
+++ App.js
@@ -1,65 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from "react";
+import "react-native-gesture-handler";
+import React, { Fragment, useEffect, useState, useCallback } from "react";
+//import {StyleSheet, View, Image, Text} from 'react-native';
 import {
 	SafeAreaView,
 	StatusBar,
-	StyleSheet,
-	Text,
-	useColorScheme,
 	View,
+	Text,
+	Keyboard,
+	KeyboardEvent,
+	Button,
+	Alert,
+	AppRegistry,
 } from "react-native";
-
-import {
-	Colors,
-	DebugInstructions,
-	Header,
-	LearnMoreLinks,
-	ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
+// import {UserContextProvider} from '~/Contexts/UserContext';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import messaging from '@react-native-firebase/messaging';
+// import NaviController from '~/Navigations/NaviController';
 
 const App = () => {
-	const isDarkMode = useColorScheme() === "dark";
+	if (Platform.OS === "android") {
+		StatusBar.setTranslucent(true);
+		StatusBar.setBackgroundColor("transparent");
+	}
 
-	const backgroundStyle = {
-		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-	};
+	const StatusBarStyle =
+		Platform.OS === "ios" ? "dark-content" : "light-content";
+	// const StatusBarColor = Platform.OS === 'ios' ? 'transparent' : 'transparent';
 
+	//  ios 키보드 영역
+	const [keyboardHeight, setKeyboardHeight] = useState(0);
+	const [isShow, setIsShow] = useState(false);
+	function onKeyboardDidShow(e) {
+		setKeyboardHeight(e.endCoordinates.height);
+		setIsShow(true);
+	}
+	function onKeyboardDidHide() {
+		setKeyboardHeight(-30);
+		setIsShow(false);
+	}
+	useEffect(() => {
+		// foregroundListener();
+
+		// checkforUpdates()
+		// AppEventsLogger.logEvent(`Test-${Platform.OS ? 'android' : 'ios'}`);
+		const showSubscription = Keyboard.addListener(
+			"keyboardDidShow",
+			onKeyboardDidShow
+		);
+		const hideSubscription = Keyboard.addListener(
+			"keyboardDidHide",
+			onKeyboardDidHide
+		);
+		// handlePushToken();
+		// saveDeviceToken();
+		return () => {
+			showSubscription.remove();
+			hideSubscription.remove();
+		};
+	}, []);
 	return (
-		<SafeAreaView style={backgroundStyle}>
-			<StatusBar
-				barStyle={isDarkMode ? "light-content" : "dark-content"}
-			/>
-			<View
-				style={{ flex: 1, backgroundColor: "white", color: "white" }}
-			></View>
-		</SafeAreaView>
+		<Fragment>
+			{/* <UserContextProvider> */}
+			<SafeAreaView style={[{ flex: 1 }]}>
+				<StatusBar
+					barStyle={StatusBarStyle}
+					backgroundColor={"transparent"}
+				/>
+				{/* <NaviController /> */}
+			</SafeAreaView>
+			{/* ios 키보드 영역 */}
+			{/* </UserContextProvider> */}
+		</Fragment>
 	);
 };
-
-const styles = StyleSheet.create({
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: "600",
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: "400",
-	},
-	highlight: {
-		fontWeight: "700",
-	},
-});
 
 export default App;
