3074944ae99139bc7707cd1d06ee48b3955e9d7a
HEO <wjddms6096@24n.co.kr>
2022-11-10 23:30:58 +0900

    메인화면 작업중

--- src/screen/user/MAIN01.js
+++ src/screen/user/MAIN01.js
@@ -5,7 +5,9 @@ import {
 	Text,
 	View,
 	Dimensions,
-	Animated,
+	Image,
+	ImageBackground,
+	Pressable,
 } from 'react-native';
 
 import Swiper from 'react-native-swiper'
@@ -18,60 +20,110 @@ export default ({navigation}) => {
 	const bgColorList = ['#CAE8B2', '#F6EF50'];
 	const [swipeIndex, setSwipeIndex] = useState(0)
 
+	useEffect(() => {}, [])
+
     return(
 	<AnimatedBackgroundColorView  
 		color={bgColorList[swipeIndex]}
 		initialColor={swipeIndex == 0 ? bgColorList[1] : bgColorList[0]}
-		duration={500}
+		duration={1000}
 		style={styles.body}
 		>
 		<Swiper 
-		style={[styles.wrapper]} 
-		showsButtons={false} 
-		loop={false} 
-		index={0}
-		onIndexChanged={(index) => {setSwipeIndex(index)}}
-		showsPagination={false}
-		width={winWidth*0.9}
-		// overflow={'visible'}
-		scrollViewStyle={{overflow: 'visible'}} removeClippedSubviews={false}
-		>
-			<View style={styles.slideItem}>
-				<View style={styles.contentsArea}>
-					<Text style={styles.text}>오늘뭐하지</Text>
-				</View>
+			style={[styles.wrapper]} 
+			showsButtons={false} 
+			loop={false} 
+			index={0}
+			onIndexChanged={(index) => {setSwipeIndex(index)}}
+			showsPagination={false}
+			width={winWidth*0.9}
+			// overflow={'visible'}
+			scrollViewStyle={{overflow: 'visible'}} removeClippedSubviews={false}
+			>
+			<View 
+				style={styles.slideItem}>
+				<ImageBackground 
+					source={require("../../Images/MAIN01_bg1.png")}
+					resizeMode="contain"
+					style={styles.contentsArea}>
+					<Pressable 
+						onPress={() => {alert('PLAY')}}
+						style={styles.contentsBtn}>
+						<Image
+						style={{height: '100%'}}
+						source={require("../../Images/MAIN01_btn1.png")}/>
+					</Pressable>
+					<Pressable style={styles.contentsText}><Text>취향 검사 하러가기</Text></Pressable>
+				</ImageBackground>
 			</View>
-			<View style={styles.slideItem}>
-				<View style={styles.contentsArea}>
-					<Text style={styles.text}>오늘뭐먹지</Text>
-				</View>
+			<View 
+				style={styles.slideItem}>
+				<ImageBackground 
+					source={require("../../Images/MAIN01_bg2.png")}
+					resizeMode="contain"
+					style={styles.contentsArea}>
+					<Pressable 
+						onPress={() => {alert('PLAY')}}
+						style={styles.contentsBtn}>
+						<Image
+						style={{height: '100%'}}
+						source={require("../../Images/MAIN01_btn2.png")}/>
+					</Pressable>
+					<Pressable style={styles.contentsText}><Text>달달러버 OH_123456</Text></Pressable>
+				</ImageBackground>
 			</View>
       	</Swiper>
+		<Pressable style={styles.cardCntArea}><Text>보유 횟수</Text></Pressable>
 	</AnimatedBackgroundColorView>
 	)
 }
-
+ 
 const styles = StyleSheet.create({
 	// #CAE8B2
 	// #F6EF50
 	body: {
 		flex: 1,
-		alignItems: 'center',
+		alignItems: 'flex-start',
 		justifyContent: 'center',
 	},
 	slideItem: {
-		marginTop: '10%',
-		height: '90%',
-		// flex: 1,
+		width: '95%',
+		// height: '80%',
+		marginTop: '12%',
+		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'flex-start',
+		alignItems: 'center',
+		alignItems: 'center',
+		marginLeft: '5%',
 	},
 	contentsArea: {
-		width: '92%',
-		height: '90%',
+		width: '98%',
+		height: '98%',
 		backgroundColor: '#fff',
 		borderRadius: 10,
 		alignItems:'center',
-		justifyContent: 'center'
+		// justifyContent: 'center',
+		shadowColor: '#000',
+		shadowOpacity: 0.2,
+		shadowRadius: 5,
+		shadowOffset: {
+			height: 0,
+			width: 0,
+		},
+		elevation: 3,
+		overflow: 'hidden'
 	},
+	contentsBtn: {
+		height: '25%',
+		top: '45%',
+	},
+	contentsText: {
+		top: '55%'
+	},
+	cardCntArea: {
+		width: '100%',
+		height: '8%',
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
   })
