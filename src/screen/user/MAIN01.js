38476369869e861f198ce942dd5062c50107362c
HEO <wjddms6096@24n.co.kr>
2022-11-10 22:12:04 +0900

    스와이프 배경 색 변경 기능

--- src/screen/user/MAIN01.js
+++ src/screen/user/MAIN01.js
@@ -1,32 +1,36 @@
 import React, {Component, useState, useEffect, useRef} from 'react';
 import {
-	SafeAreaView,
-	ScrollView,
 	StatusBar,
 	StyleSheet,
 	Text,
-	useColorScheme,
 	View,
 	Dimensions,
+	Animated,
 } from 'react-native';
 
 import Swiper from 'react-native-swiper'
+import { AnimatedBackgroundColorView } from 'react-native-animated-background-color-view';
+
 
 export default ({navigation}) => {
 	const winWidth = Dimensions.get("window").width;
 
-	useEffect(() => {
-
-	},[])
+	const bgColorList = ['#CAE8B2', '#F6EF50'];
+	const [bgColorIndex, setBbgColorIndex] = useState(true)
 
     return(
-	<View style={styles.body}>
+	<AnimatedBackgroundColorView  
+		color={bgColorList[bgColorIndex ? 0 : 1]}
+		initialColor={bgColorList[bgColorIndex ? 1 : 0]}
+		duration={300}
+		style={styles.body}
+		>
 		<Swiper 
 		style={[styles.wrapper, {borderWidth: 1}]} 
 		showsButtons={false} 
-		// loop={false} 
+		loop={false} 
 		index={0}
-		onIndexChanged={() => {}}
+		onIndexChanged={() => {setBbgColorIndex(!bgColorIndex)}}
 		showsPagination={false}
 		width={winWidth*0.9}
 		// overflow={'visible'}
@@ -43,33 +47,39 @@ export default ({navigation}) => {
 				</View>
 			</View>
       	</Swiper>
-	</View>
+	</AnimatedBackgroundColorView>
 	)
 }
 
 const styles = StyleSheet.create({
+	// #CAE8B2
+	// #F6EF50
 	body: {
+		flex: 1,
 		width: '100%',
 		height: '100%',
 		alignItems: 'center',
+		justifyContent: 'center',
 	},
 	slide: {
-		flex: 1,
+		marginTop: '10%',
+		height: '90%',
+		// flex: 1,
 		justifyContent: 'center',
 		alignItems: 'flex-start',
+		// backgroundColor: '#fff',
 	},
 	slide1: {
-		width: '95%',
-		height: '100%',
-		backgroundColor: '#CAE8B2',
+		width: '92%',
+		height: '90%',
+		borderWidth:1
 	},
 	slide2: {
-		width: '95%',
-		height: '100%',
-		backgroundColor: '#F6EF50',
+		width: '92%',
+		height: '90%',
+		borderWidth:1
 	},
 		text: {
-		color: '#fff',
 		fontSize: 30,
 		fontWeight: 'bold'
 	}
