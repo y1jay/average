4634d3c20666b30449df6c0313c77d6fac31bc3e
HEO <heo940122@gmail.com>
2022-12-06 02:08:09 +0900

    프로필 설정 팝업

--- src/Components/ProfileSetting.js
+++ src/Components/ProfileSetting.js
@@ -18,6 +18,7 @@ import {
 
 import { UserGetter, UserSetter, UserRemover } from "../User/UserInfo";
 import commonStyles from './Style';
+import { TextInput } from "react-native-gesture-handler";
 
 export default ({navigation, modalVisible, setModalVisible}) => {
 	return (
@@ -31,28 +32,45 @@ export default ({navigation, modalVisible, setModalVisible}) => {
                 style={styles.modalBody}>
                 <Pressable style={styles.settingItemArea} onPress={() => {setModalVisible(false)}}>
                     <Image style={{width: 20, height: 20}} source={require('../Images/back_arrow.png')} resizeMode={'contain'}/>
+                    <Text style={styles.profileSettingTitle}>프로필 설정</Text>
                 </Pressable>
-                <Text style={styles.settingTitle}>프로필 설정</Text>
-
-                <Pressable onPress={() => {setModalVisibleProfileSetting(true)}}>
-                    <ImageBackground 
-                        source={require('../Images/profile.png')}
-                        style={styles.myInfoImg}
-                        resizeMode="cover">
-                    </ImageBackground>
-                </Pressable>
-                
+                <View style={styles.myInfoImgArea}>
+                    <Pressable onPress={() => {alert('프로필 이미지 설정')}}>
+                        <ImageBackground 
+                            source={require('../Images/profile.png')}
+                            style={styles.myInfoImg}
+                            resizeMode="cover">
+                        </ImageBackground>
+                    </Pressable>
+                </View>
                 <Text style={styles.settingTitle}>닉네임</Text>
+                <View style={styles.settingInputArea}>
+                    <TextInput  style={styles.settingInput}/>
+                </View>
                 <Text style={styles.settingTitle}>칭호</Text>
+                <View style={styles.settingInputArea}>
+                    <Text>칭호1</Text>
+                    <Text>칭호2</Text>
+                </View>
+                <View style={styles.absoluteBtnArea}>
+                    <Pressable style={styles.absoluteBtn}>
+                        <Text style={styles.absoluteBtnText}>저장하기</Text>
+                    </Pressable>
+                </View>
             </View>
         </Modal>
     )
 }
 const styles = StyleSheet.create({
     modalBody: {
-        backgroundColor: '#F8F8F8',
+        backgroundColor: '#FFF',
         flexGrow: 1,
 
+    },
+    profileSettingTitle: {
+        fontWeight: 'bold',
+        marginLeft: 15,
+        fontSize: 14
     },
     settingTitle: {
         padding: 10,
@@ -62,15 +80,53 @@ const styles = StyleSheet.create({
     },
     settingItemArea: {
         padding: 20,
-        borderBottomWidth: 1,
-        borderColor: '#F8F8F8',
-        backgroundColor: '#FFF',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    myInfoImgArea: {
+        alignItems: 'center'
     },
 	myInfoImg: {
 		width: 100,
 		height: 100,
 		borderRadius: 46,
 		backgroundColor: '#FFF',
-		overflow: 'hidden'
+		overflow: 'hidden',
+		shadowColor: "#212121",
+		shadowOpacity: 0.2,
+		shadowRadius: 3,
+		shadowOffset: {
+			height: 2,
+			width: 0,
+		},
+		elevation: 3,
 	},
+    settingInputArea: {
+        paddingLeft: 20,
+        paddingRight: 20
+    },
+    settingInput: {
+        backgroundColor: '#f8f8f8',
+        padding: 10,
+        borderRadius: 10
+    },
+    absoluteBtnArea: {
+        width: '100%',
+        padding: 20,
+        paddingTop: 10,
+        paddingBottom: 15,
+        position: 'absolute',
+        bottom: 0,
+    },
+    absoluteBtn: {
+        backgroundColor: '#18A8C8',
+        borderRadius: 10,
+        padding: 15,
+        alignItems: 'center'
+    },
+    absoluteBtnText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
 })
